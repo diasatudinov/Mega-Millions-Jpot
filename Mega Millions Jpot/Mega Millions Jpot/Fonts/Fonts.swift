135f0ff6b06c6b822b2c1a897afd41c60545ec44
Dias <dias.atudinov@gmail.com>
2025-01-09 17:09:17 +0500

    Managers

--- Mega Millions Jpot/Mega Millions Jpot/Fonts/Fonts.swift
+++ Mega Millions Jpot/Mega Millions Jpot/Fonts/Fonts.swift
@@ -1,5 +1,13 @@
+//
+//  Fonts.swift
+//  Mega Millions Jpot
+//
+//  Created by Dias Atudinov on 09.01.2025.
+//
+
+
 import Foundation
 
 enum Fonts: String {
-    case regular = "ChauPhilomeneOne-Regular"
+    case regular = "Caprasimo-Regular"
 }
--- Mega Millions Jpot/Mega Millions Jpot/Managers/DeviceInfo.swift
+++ Mega Millions Jpot/Mega Millions Jpot/Managers/DeviceInfo.swift
@@ -1,3 +1,11 @@
+//
+//  DeviceInfo.swift
+//  Mega Millions Jpot
+//
+//  Created by Dias Atudinov on 09.01.2025.
+//
+
+
 import UIKit
 
 class DeviceInfo {
