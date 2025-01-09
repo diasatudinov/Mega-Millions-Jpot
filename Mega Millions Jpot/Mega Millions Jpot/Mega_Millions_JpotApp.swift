c6cdd74c2a5d18c1cd74010eac6e23605dfc6adc
Dias <dias.atudinov@gmail.com>
2025-01-09 15:35:34 +0500

    first commit

---
+++ Mega Millions Jpot/Mega Millions Jpot/Mega_Millions_JpotApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mega_Millions_JpotApp.swift
+//  Mega Millions Jpot
+//
+//  Created by Dias Atudinov on 09.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct Mega_Millions_JpotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
